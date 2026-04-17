2cdf580656f71d68eb6729078c6db98c37dc7b12
Cesar Orellana <cesarlorellana184@gmail.com>
2026-04-16 21:06:07 -0300

    punto2 Tp2

--- Tp1-Algoritmo/Lista.h
+++ Tp1-Algoritmo/Lista.h
@@ -17,68 +17,68 @@ typedef struct{
 
 typedef struct nodo *NLista;
 
-Lista crearLista(){
-    Lista newLista;
-    newLista.L = NULL;
-    newLista.cantidad = 0;
-    return newLista;
+Lista crearLista(){             // O(1)
+    Lista newLista;         // O(1)
+    newLista.L = NULL;      // O(1)
+    newLista.cantidad = 0;  // O(1)
+    return newLista;        // O(1)
 }
 
-void mostrar(Lista H){
-    while (H.L != NULL)
+void mostrar(Lista H){                  // O(n)
+    while (H.L != NULL)             // *n IT (n = cant nodos) -> O(n)
     {
-        printf("%c", H.L->dato);
-        H.L = H.L->Siguiente; 
+        printf("%c", H.L->dato);    // O(1)
+        H.L = H.L->Siguiente;       // O(1)
     }
 }
 
-bool esListaVacia(Lista H){
-    if(H.L == NULL) return true;
-    return false;
+bool esListaVacia(Lista H){                 // O(1)
+    if(H.L == NULL) return true;        // condición O(1)
+    return false;                       // O(1)
 }
 
-int longitud(Lista H){
-    int longitud = 0;
-    while (H.L != NULL)
+int longitud(Lista H){                      // O(n)
+    int longitud = 0;                   // O(1)
+    while (H.L != NULL)         // *n IT (n cant Nodos) -> O(n)
     {
-        longitud++;
-        H.L = H.L->Siguiente;
+        longitud++;             // O(1)
+        H.L = H.L->Siguiente;   // O(1)
     }
-    return longitud;
+    return longitud;                    // O(1)
 }
 
-item primerElemento(Lista H){
-    if(esListaVacia(H)) return indefinido;
-    return H.L->dato;
+item primerElemento(Lista H){                       // O(1)
+    if(esListaVacia(H)) return indefinido;      // condición O(1)
+    return H.L->dato;                           // O(1)
 }
 
-Lista insertar(Lista H, item dato){
-    nodo *newNodo = new nodo;
-    newNodo->dato = dato;
-    newNodo->Siguiente = H.L;
-    H.L = newNodo;
-    H.cantidad = H.cantidad + 1;
-    return H;
+Lista insertar(Lista H, item dato){         // O(!)
+    nodo *newNodo = new nodo;       // O(1)
+    newNodo->dato = dato;           // O(1)
+    newNodo->Siguiente = H.L;       // O(1)
+    H.L = newNodo;                  // O(1)
+    H.cantidad = H.cantidad + 1;    // O(1)
+    return H;                       // O(1)
 }
 
-Lista borrar(Lista H){
-    if(esListaVacia(H)) return H;
-    Lista ListAux = H;
-    struct nodo *N_Aux = ListAux.L; 
-    ListAux.L = N_Aux->Siguiente;
-    ListAux.cantidad == ListAux.cantidad - 1;
-    delete(N_Aux);
-    return ListAux;
+Lista borrar(Lista H){                  // -- O(1)
+    if(esListaVacia(H)) return H;     // condición O(1)
+    Lista ListAux = H;                  // O(1)
+    struct nodo *N_Aux = ListAux.L;     // O(1)
+    ListAux.L = N_Aux->Siguiente;       // O(1)
+    ListAux.cantidad == ListAux.cantidad - 1;   //O(1)
+    delete(N_Aux);                      // O(1)
+    return ListAux;                     // O(1)
 }
 
-bool pertenece(Lista H, item dato){
-    nodo *Aux = H.L;
-    if (Aux == NULL) return false;
+bool pertenece(Lista H, item dato){                                 // O(n)
+    nodo *Aux = H.L;                                    // O(1)
+    if (Aux == NULL) return false;                      // condición O(1), O(1)
     
-    while(Aux->Siguiente != NULL && Aux->dato != dato){
-        Aux = Aux->Siguiente;
+    while(Aux->Siguiente != NULL && Aux->dato != dato){ // *n IT , (n = Cant nodos) -> O(n) 
+        Aux = Aux->Siguiente;                       // O(1)
     }
-    return Aux->dato == dato;
+    return Aux->dato == dato;                       // O(1)
 }
 
 Lista insertarFinal(Lista H, item dato){
