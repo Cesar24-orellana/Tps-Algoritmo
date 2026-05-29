bb1837b3ed6e0f579b6b3792ec2c27afdcd0ff0c
Cesar Orellana <cesarlorellana184@gmail.com>
2026-05-29 20:24:01 -0300

    agregando funciones

--- Tp4/Lista.h
+++ Tp4/Lista.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 typedef char item;
+const item indefinido = '@';
 
 typedef struct Nodo
 {
@@ -11,7 +12,58 @@ typedef struct Nodo
 
 typedef struct
 {
-    int capacidad;
+    int ocupado;
     int capacidadMax;
-    Nodo *L;
+    Nodo *objetos;
+    Nodo *primero;
 }Baul;
+
+Baul BaulVacio(int capacidad){
+    Baul baulVacio;
+    baulVacio.capacidadMax = capacidad;
+    baulVacio.ocupado = 0;
+    baulVacio.objetos = NULL;
+    baulVacio.primero = NULL;
+    return baulVacio;
+}
+
+void Guardar(Baul *B, item dato){
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = dato;
+    nuevo->siguiente = B->objetos;
+    B->objetos = nuevo;
+    B->ocupado++;
+    if(B->primero == NULL) B->primero = nuevo;
+}
+
+void Agregar(Baul *B,item dato){
+    if(Disponible(*B)>0) Guardar(B, dato);
+}
+
+int Capacidad(Baul B){
+    return B.capacidadMax;
+}
+
+bool EstaVacio(Baul B){
+    return B.objetos == NULL;
+}
+
+item Ultimo(Baul B){
+    if(EstaVacio(B)) return indefinido;
+    return B.objetos->dato;
+}
+
+item Primero(Baul B){
+    if(EstaVacio(B)) return indefinido;
+    return B.primero->dato;
+}
+
+void QuitarUltimo(Baul *B){
+    if(!EstaVacio(*B)){
+        Nodo *aux=B->objetos;
+        if (B->objetos == B->primero) B->primero = NULL;
+        B->objetos = aux->siguiente;
+        B->ocupado--;
+        delete(aux);
+    }
+}
