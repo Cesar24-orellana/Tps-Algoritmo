e961cc3cdfbff6ac3e97dbfd3eb01eb722e93ab3
Cesar Orellana <cesarlorellana184@gmail.com>
2026-06-07 17:16:54 -0300

    iniciando tp6

---
+++ Tp6/Fila.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+const Item Indefinido = -9999;
+
+typedef struct Nodo
+{
+    Item dato;
+    struct Nodo *siguiente;
+}Nodo;
+
+typedef struct
+{
+    int cantidad;
+    Nodo *final;
+    Nodo *frente;
+}Fila;
+
+// - - - - - - Constructores - - - - - -
+
+Fila FilaVacia(){
+    Fila nueva;
+    nueva.cantidad = 0;
+    nueva.final = NULL;
+    nueva.frente = NULL;
+    return nueva;
+}
+
+bool esFilaVacia(Fila F){
+    return (F.final == NULL && F.frente == NULL);
+}
+
+void enfila(Fila *F, Item dato){
+    Nodo *nuevo = new Nodo;
+    nuevo->dato = dato;
+    nuevo->siguiente = NULL;
+    F->cantidad++;
+    if(esFilaVacia(*F)){
+        F->final = nuevo;
+        F->frente = nuevo;
+    } else
+    {
+        F->final->siguiente = nuevo;
+    }
+}
+
+Item frente(Fila F){
+    if(esFilaVacia(F)) return Indefinido;
+    return F.frente->dato;
+}
+
+Fila defila(Fila F){
+    if(esFilaVacia(F)) return FilaVacia();
+    Nodo *eliminar = F.frente;
+    if(F.frente == F.final) return FilaVacia();
+    F.frente = F.frente->siguiente;
+    F.cantidad--;
+    delete(eliminar);
+    return F;
+}
