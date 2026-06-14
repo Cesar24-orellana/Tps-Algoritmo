d43f7aafe432996d0efa955e63be07edf42bc313
Cesar Orellana <cesarlorellana184@gmail.com>
2026-06-14 17:39:32 -0300

    Modificando ab

--- Tp7/AB.h
+++ Tp7/AB.h
@@ -6,38 +6,58 @@ const Item Indefinido = '@';
 
 typedef struct Nodo
 {
-    Item Dato;
-    struct Nodo *siguiente;
+    Item Raiz;
+    struct Nodo *Izq;
+    struct Nodo *Der;
 }Nodo;
 
-typedef struct
-{
-    int cantidad;
-    Item Raiz;
-    Nodo *Izq;
-    Nodo *Dere;
-}AB;
+typedef struct Nodo * AB;
 
 //      Constructora y test
 
 AB ABVacio(){
-    AB nuevo;
-    nuevo.cantidad = 0;
-    nuevo.Izq = NULL;
-    nuevo.Dere = NULL;
-    nuevo.Raiz = ' ';
-    return nuevo;
+    return NULL;
 }
 
 bool esABVacio(AB ab){
-    return (ab.Dere == NULL && ab.Izq == NULL);
+    return (ab->Der == NULL && ab->Izq == NULL);
 }
 
-AB armarAB(Nodo izq, Item x, Nodo der){     // NO ME GUSTA NADAAAAAA 😓
-    AB nuevo;
-    nuevo.cantidad++;
-    nuevo.Dere = &der;
-    nuevo.Izq = &izq;
-    nuevo.Raiz = x;
+AB armarAB(AB izq, Item x, AB der){
+    Nodo *nuevo = new Nodo;
+    nuevo->Raiz = x;
+    nuevo->Izq = izq;
+    nuevo->Der = der;
     return nuevo;
+}
+
+AB Izquierdo(AB ab){
+    return ab->Izq;
+}
+
+Item Raiz(AB ab){
+    if(esABVacio(ab)) return Indefinido;
+    return ab->Raiz;
+}
+
+AB Derecha(AB ab){
+    return ab->Der;
+}
+
+bool pertenece(AB ab, Item x){
+    if(esABVacio(ab)) return false;
+    if(x = Raiz(ab)) return true;
+    return pertenece(Izquierdo(ab), x) || pertenece(Derecha(ab), x);
+}
+
+bool Iguales(AB ab1, AB ab2){
+    if(esABVacio(ab1) && esABVacio(ab2)) return true;
+    if(Raiz(ab1) == Raiz(ab2)) return Iguales(Izquierdo(ab1), Izquierdo(ab2)) && Iguales(Derecha(ab1), Derecha(ab2));
+    return false;
+}
+
+AB PodarHojas(AB ab){
+    if(esABVacio(ab)) return ABVacio();
+    if(esABVacio(Izquierdo(ab)) && esABVacio(Derecha(ab))) return ABVacio();
+    return armarAB(PodarHojas(Izquierdo(ab)), Raiz(ab), PodarHojas(Derecha(ab)));
 }
